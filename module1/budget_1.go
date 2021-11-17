190175f2a57a0c3ae77fd40bd511388fa5184b37
Aaron Lael <alael@utah.gov>
2021-11-17 09:59:45 -0700

    corrected array of item

--- module1/budget_1.go
+++ module1/budget_1.go
@@ -2,11 +2,12 @@ package module1
 
 // Budget stores budget information
 type Budget struct {
-  Max   float32
-  Items Item[]
+	Max   float32
+	Items []Item
 }
+
 // Item stores item information
 type Item struct {
-  Description string
-  Price       float32
+	Description string
+	Price       float32
 }
